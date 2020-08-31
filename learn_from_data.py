635a15b0d25c8972cdf39b24d7c199682e8ec1e8
Mike Zhang <mikejj.zhang@gmail.com>
2020-08-31 18:00:27 +0200

    Add typing. Reformat code.

--- learn_from_data.py
+++ learn_from_data.py
@@ -1,61 +1,76 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-'''
-This script reads a saved numpy array with features prepared for sklearn.
-The features are then used to learn something from the data.
-'''
+#This script reads a saved numpy array with features prepared for sklearn.
+#The features are then used to learn something from the data.
 
-__author__ = "Johannes Bjerva, and Malvina Nissim (modified by Mike Zhang)"
+__author__ = "Johannes Bjerva, and Malvina Nissim"
 __credits__ = ["Johannes Bjerva", "Malvina Nissim"]
 __license__ = "GPL v3"
-__version__ = "0.2"
-__maintainer__ = "Johannes Bjerva"
-__email__ = "j.bjerva@rug.nl"
-__status__ = "early alpha"
+__version__ = "0.3 (31/08/2020)"
+__maintainer__ = "Mike Zhang"
+__email__ = "mikz@itu.dk"
+__status__ = "Testing"
 
 import argparse
+import logging
 import random
-import numpy as np
 from collections import Counter
-from sklearn.tree import DecisionTreeClassifier
+from typing import List, Tuple, Union
+
+import pandas as pd
+import numpy as np
+from sklearn.metrics import (accuracy_score, classification_report,
+                             confusion_matrix)
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 from sklearn.svm import LinearSVC
+from sklearn.tree import DecisionTreeClassifier
 
-def read_features(fname):
+logging.basicConfig(format='%(levelname)s %(message)s', level=logging.DEBUG)
+
+
+
+def read_features(fname: str) -> Tuple[np.ndarray, np.ndarray]:
     with open(fname, 'rb') as in_f:
         loaded = np.load(in_f)
+
         return loaded['X'], loaded['y']
 
-def make_splits(X, y, args):
-    X = list(X) # TODO: Why does this have to be a list?
-    train = float(args.split[0])/100.0
-    test = float(args.split[1])/100.0
+def make_splits(X: np.ndarray, 
+                y: np.ndarray, 
+                args: argparse.Namespace) -> Tuple[List, List, List, List]:
+    X: list = list(X)
+    y: list = list(y)
+
+    train: float = float(args.split[0])/100.0
+    test: float = float(args.split[1])/100.0
+
     combined = list(zip(X, y))
     random.shuffle(combined)
     X[:], y[:] = zip(*combined)
-    dev_split  = int(len(y) * train)
-    test_split = dev_split + int(len(y) * test)
 
-    train_X = X[:dev_split]
-    train_y = y[:dev_split]
-    dev_X = X[dev_split:test_split]
-    dev_y = y[dev_split:test_split]
-    test_X  = X[test_split:]
-    test_y  = y[test_split:]
+    train_split = int(len(y) * train)
+    dev_split = int((len(y) * train) * test)
+
+    train_X = X[:train_split]
+    train_y = y[:train_split]
+    dev_X = train_X[:dev_split]
+    dev_y = train_y[:dev_split]
+    test_X  = X[train_split:]
+    test_y  = y[train_split:]
 
     return train_X, train_y, dev_X, dev_y, test_X, test_y
 
-def baseline(train_y, test_y):
+def baseline(train_y: List[Union[int, str]], test_y: List[Union[int, str]]) -> None:
     most_common = Counter(train_y).most_common()[0][0]
     baseline = sum([1 for label in test_y if label == most_common]) / float(len(test_y))
-    print('Most frequent label:\t{0}'.format(most_common))
-    print('Baseline accuracy:\t{0}'.format(baseline))
+    logging.info(f'Most frequent label: {most_common}')
+    logging.info(f'Baseline accuracy: {baseline}')
 
-def get_classifiers(args):
+def get_classifiers(args: argparse.Namespace) -> List[object]:
     classifiers = []
+
     if 'nb' in args.algorithms:
         classifiers.append(MultinomialNB())
     if 'dt' in args.algorithms:
@@ -71,13 +86,17 @@ def get_classifiers(args):
 
     return classifiers
 
-def evaluate_classifier(clf, test_X, test_y, args):
+def evaluate_classifier(clf: object, 
+                        test_X: List[Union[int, str]], 
+                        test_y: List[Union[int, str]], 
+                        args: argparse.Namespace) -> None:
     preds = clf.predict(test_X)
     accuracy = accuracy_score(preds, test_y)
 
-    print('Accuracy: {0} ({1})'.format(accuracy, str(clf)))
     if args.cm or args.plot:
         show_confusion_matrix(test_y, preds, args)
+
+    return f'Accuracy: {accuracy}, classifier: {clf}'
 
 def show_confusion_matrix(test_y, pred_y, args):
     cm = confusion_matrix(test_y, pred_y, labels=sorted(list(set(test_y))))
@@ -86,16 +105,16 @@ def show_confusion_matrix(test_y, pred_y, args):
         cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
     np.set_printoptions(precision=2)
 
-    print('Confusion matrix')
+    logging.debug('Showing Confusion Matrix')
     if args.cm:
-        print(cm)
+        print(f'\n{pd.DataFrame(cm, index=sorted(list(set(test_y))), columns=sorted(list(set(test_y))))}\n')
     if args.plot:
         from plotting import plot_confusion_matrix # Import here due to potential matplotlib issues
         plot_confusion_matrix(cm, test_y)
 
-    print(classification_report(test_y, pred_y, sorted(list(set(test_y)))))
+    print(classification_report(test_y, pred_y, labels=sorted(list(set(test_y)))))
 
-random.seed(1337)
+# random.seed(1337)
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--npz', help='feature npz filename', type=str)
@@ -106,7 +125,7 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     X, y = read_features(args.npz)
-    train_X, train_y, dev_X, dev_y, test_X, test_y = make_splits(X, y)
+    train_X, train_y, dev_X, dev_y, test_X, test_y = make_splits(X, y, args)
     baseline(train_y, test_y)
     classifiers = get_classifiers(args)
 
