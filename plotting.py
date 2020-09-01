43c47095d172cc9933d70f7b9c1911158f21f989
Mike Zhang <mikejj.zhang@gmail.com>
2020-09-01 21:20:06 +0200

    Add typing

--- plotting.py
+++ plotting.py
@@ -9,14 +9,18 @@ __author__ = "Johannes Bjerva, and Malvina Nissim (modified by Mike Zhang)"
 __credits__ = ["Johannes Bjerva", "Malvina Nissim"]
 __license__ = "GPL v3"
 __version__ = "0.2"
-__maintainer__ = "Johannes Bjerva"
-__email__ = "j.bjerva@rug.nl"
+__maintainer__ = "Mike Zhang"
+__email__ = "mikz@itu.dk"
 __status__ = "early alpha"
 
-import matplotlib.pyplot as plt
+from typing import List
+
 import numpy as np
 
-def plot_confusion_matrix(cm, test_y, title='Confusion matrix', cmap=plt.cm.Blues):
+import matplotlib.pyplot as plt
+
+
+def plot_confusion_matrix(cm: np.ndarray, test_y: List[str], title='Confusion matrix', cmap=plt.cm.Blues):
     plt.figure()
     plt.imshow(np.vstack((cm, np.zeros(cm.shape[0]))), interpolation='nearest', cmap=cmap)
     plt.title(title)
